3314582519b61cff8517ab4bcfb6fda88317043a
Peter Edge <peter.edge@gmail.com>
2015-04-13 17:29:58 +0200

    fix

--- blocking_read_write_closer.go
+++ blocking_read_write_closer.go
@@ -60,5 +60,6 @@ func (this *blockingReadWriteCloser) Close() error {
 	if !this.closed.CompareAndSwap(false, true) {
 		return errors.New("concurrent: already called close")
 	}
+	this.cv.Signal()
 	return nil
 }
